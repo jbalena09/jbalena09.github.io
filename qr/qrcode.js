d873d24b43c0a72f8db5b285cca0b666930f21f9
jbalena09 <45332736+jbalena09@users.noreply.github.com>
2023-10-19 20:41:16 +0800

    Update qrcode.js

--- qr/qrcode.js
+++ qr/qrcode.js
@@ -1271,6 +1271,7 @@ var QRCode;
           this._bIsPainted = false;
           this._elImage = document.createElement("img");
           this._elImage.alt = "Scan me!";
+          this._elImage.id = "img1";
           this._elImage.style.display = "none";
           this._el.appendChild(this._elImage);
           this._bSupportDataURI = null;
